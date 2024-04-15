09e81622efb34810a9fa4a603ba37b76017b68fa
DaviEspinosa <davipedrosa037@gmail.com>
2024-04-15 15:23:04 -0300

    ok

---
+++ sa2_correcao/lib/View/LoginPageView.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PaginaLogin extends StatefulWidget {
+  const PaginaLogin({super.key});
+
+  @override
+  State<PaginaLogin>  createState() =>  _PaginaLogin
+  }
+
+class PageState extends  {
+  
+  return const Placeholder();
+}
---
+++ sa2_correcao/lib/main.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "SA2",
+//       debugShowCheckedModeBanner: false,
+//       home: PagiaLogin(),
+//     );
+//   }
+// }
