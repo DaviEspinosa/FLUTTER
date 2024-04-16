36d0df1f12a2b24461e03045a552a1a865087878
DaviEspinosa <davipedrosa037@gmail.com>
2024-04-16 13:14:01 -0300

    correção

--- sa2_correcao/lib/Controller/BancoDados.dart
+++ sa2_correcao/lib/Controller/BancoDados.dart
@@ -1,132 +1,80 @@
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sa2_correcao/Model/Usuario.dart';
+import 'package:sqflite/sqflite.dart';
 
-// class PaginaLogin extends StatefulWidget {
-//   const PaginaLogin({super.key});
+class BancoDadosCrud {
+  static const String Nome_BD = 'usuarios.db'; // Nome do banco de dados
+  static const String Nome_Tabela = 'usuarios'; // Nome da tabela
+  static const String
+      Script_Criacao_Tabela = // Script SQL para criar a tabela
+      "CREATE TABLE IF NOT EXISTS usuarios("+
+        "id SERIAL PRIMARY KEY," +
+          "u_nome TEXT, email TEXT UNIQUE," +
+          "senha TEXT)";
 
-//   @override
-//   State<PaginaLogin> createState() => _PaginaLoginState();
-// }
-
-// class _PaginaLoginState extends State<PaginaLogin> {
-//   final _formKey = GlobalKey<FormState>();
-//   TextEditingController _emailController = TextEditingController();
-//   TextEditingController _senhaController = TextEditingController();
-//   bool _loading = false;
   
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Tela de Login"),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(20.0),
-//         child: Form(
-//           key: _formKey,
-//           child: SingleChildScrollView(
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: <Widget>[
-//                 Text(
-//                   'Login',
-//                   style: TextStyle(
-//                     fontSize: 24,
-//                     fontWeight: FontWeight.bold,
-//                   ),
-//                 ),
-//                 SizedBox(height: 20),
-//                 TextFormField(
-//                   controller: _emailController,
-//                   decoration: InputDecoration(labelText: 'E-mail'),
-//                   validator: (value) {
-//                     if (value == null || value.trim().isEmpty) {
-//                       return 'Por favor, insira seu e-mail';
-//                     } else if (!isValidEmail(value)) {
-//                       return 'E-mail inválido';
-//                     }
-//                     return null;
-//                   },
-//                   inputFormatters: [
-//                     FilteringTextInputFormatter.deny(RegExp(r'[0-9]')),
-//                   ],
-//                 ),
-//                 SizedBox(height: 20),
-//                 TextFormField(
-//                   controller: _senhaController,
-//                   decoration: InputDecoration(labelText: 'Senha'),
-//                   obscureText: true,
-//                   validator: (value) {
-//                     if (value?.trim().isEmpty ?? true) {
-//                       return 'Por favor, insira sua senha';
-//                     }
-//                     return null;
-//                   },
-//                 ),
-//                 SizedBox(height: 20),
-//                 _loading
-//                     ? CircularProgressIndicator()
-//                     : ElevatedButton(
-//                         onPressed: _login,
-//                         child: Text('Acessar'),
-//                       ),
-//                 SizedBox(height: 20),
-//                 TextButton(
-//                   onPressed: () {
-//                     Navigator.push(
-//                       context,
-//                       MaterialPageRoute(builder: (context) => PaginaCadastro()),
-//                     );
-//                   },
-//                   child: Text('Não tem uma conta? Cadastre-se'),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
+  Future<Database> _getDatabase() async {
+    return openDatabase(
+      join(
+          await getDatabasesPath(), Nome_BD), // Caminho do banco de dados
+      onCreate: (db, version) {
+        return db.execute(
+            Script_Criacao_Tabela); // Executa o script de criação da tabela quando o banco é criado
+      },
+      version: 1,
+    );
+  }
+  // Método para criar um novo contato no banco de dados
+  Future<void> create(Usuario usuario) async {
+    try {
+      final Database db = await _getDatabase();
+      await db.insert(
+          Nome_Tabela, usuario.toMap()); // Insere o contato no banco de dados
+    } catch (ex) {
+      print(ex);
+      return;
+    }
+  }
 
-//   void _login() async {
-//     if (_formKey.currentState!.validate()) {
-//       String email = _emailController.text;
-//       String senha = _senhaController.text;
+  // Método para obter o dado do usuario
+  Future<Usuario?> getUsuario(String email, String senha) async {
+    try {
+      final Database db = await _getDatabase();
+      final List<Map<String, dynamic>> maps =
+          await db.query(Nome_Tabela,
+          where: 'email = ? AND senha = ?',
+          whereArgs: [email,senha]
+          ); // Consulta todos os contatos na tabela
 
-//       setState(() {
-//         _loading = true;
-//       });
+      if (maps.isNotEmpty){
+        return Usuario.fromMap(maps[0]);
+      }else{
+        return null;
+      }
+    } catch (ex) {
+      print(ex);
+      return null;
+    }
+  }
+  // Método para verificar existência do usuario
+  Future<bool> existsUsuario(String email, String senha) async {
+    try {
+      final Database db = await _getDatabase();
+      final List<Map<String, dynamic>> maps =
+          await db.query(Nome_Tabela,
+          where: 'email = ? AND senha = ?',
+          whereArgs: [email,senha]
+          ); // Consulta todos os contatos na tabela
 
-//       BancoDadosCrud bancoDados = BancoDadosCrud();
-//       try {
-//         Usuario usuario = await bancoDados.getusuario(email, senha);
-//         if (usuario != null) {
-//           Navigator.push(
-//             context,
-//             MaterialPageRoute(
-//               builder: (context) => ConfiguracoesPage(email: user.email),
-//             ),
-//           );
-//         } else {
-//           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//             content: Text('Email ou senha incorretos'),
-//           ));
-//         }
-//       } catch (e) {
-//         print('Erro durante o login: $e');
-//         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//           content: Text('Erro durante o login. Tente novamente mais tarde.'),
-//         ));
-//       } finally {
-//         setState(() {
-//           _loading = false;
-//         });
-//       }
-//     }
-//   }
-
-//   bool isValidEmail(String email) {
-//     return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
-//   }
-// }
+      if (maps.isNotEmpty){
+        return true;
+      }else{
+        return false;
+      }
+    } catch (ex) {
+      print(ex);
+      return false;
+    }
+  }
+}
--- sa2_correcao/lib/main.dart
+++ sa2_correcao/lib/main.dart
@@ -1,17 +1,20 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+import 'View/LoginPageView.dart';
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: "SA2",
-//       debugShowCheckedModeBanner: false,
-//       home: PagiaLogin(),
-//     );
-//   }
-// }
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "SA2",
+      debugShowCheckedModeBanner: false,
+      home: PaginaLogin(),
+    );
+  }
+}
