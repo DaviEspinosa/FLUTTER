987a53b37a00ebd0868fbc578cca3889feb1ebe5
DaviEspinosa <123770867+DaviEspinosa@users.noreply.github.com>
2024-06-18 08:49:00 -0300

    Update details_weather_screen.dart

--- projeto_api_geo_temp_correcao/lib/View/details_weather_screen.dart
+++ projeto_api_geo_temp_correcao/lib/View/details_weather_screen.dart
@@ -3,7 +3,7 @@ import 'package:projeto_api_geo_temp_correcao/Controller/weather_controller.dart
 
 class WeatherDetailsScreen extends StatefulWidget {
   final String cityName;
-  const WeatherDetailsScreen({super.key, required this.cityName});
+  const WeatherDetailsScreen({Key? key, required this.cityName}) : super(key: key);
 
   @override
   State<WeatherDetailsScreen> createState() => _WeatherDetailsScreenState();
@@ -14,9 +14,10 @@ class _WeatherDetailsScreenState extends State<WeatherDetailsScreen> {
 
   @override
   void initState() {
-    _controller.getWeather(widget.cityName);
+    _controller.getWeather(widget.cityName); // Carregar dados do clima ao inicializar a tela
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,43 +26,56 @@ class _WeatherDetailsScreenState extends State<WeatherDetailsScreen> {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
-              'Weather Details',
+              'Detalhes do Clima',
               style: TextStyle(
                 fontSize: 30,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 20),
-          FutureBuilder(
-            future: _controller.getWeather(widget.cityName), 
-            builder: (context, snapshot){
-              if(_controller.weatherList.isEmpty){
-                return const Center(child: CircularProgressIndicator());
-              }else{
-                return Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(_controller.weatherList.last.name),
-                        //icon favorite
-                        IconButton(
-                          icon: const Icon(Icons.favorite_border_outlined),
-                          onPressed: (){
-                            // adicionar aos favoritos
-                          },
-                        ),
-                      ],
-                    ),
-                    Text(_controller.weatherList.last.description),
-                    Text((_controller.weatherList.last.temp-273).toStringAsFixed(2)),
-                    
-                  ],
-                );
-              }
-
-            })
+            SizedBox(height: 20),
+            FutureBuilder(
+              future: _controller.getWeather(widget.cityName),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (_controller.weatherList.isEmpty) {
+                  return Center(child: Text('Dados não encontrados'));
+                } else {
+                  return Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            _controller.weatherList.last.name,
+                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.favorite_border_outlined),
+                            onPressed: () {
+                              // Adicionar aos favoritos
+                              // Implemente a lógica para adicionar aos favoritos aqui
+                            },
+                          ),
+                        ],
+                      ),
+                      Text(
+                        _controller.weatherList.last.description,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        '${(_controller.weatherList.last.temp - 273.15).toStringAsFixed(2)} °C',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  );
+                }
+              },
+            ),
           ],
         ),
       ),
